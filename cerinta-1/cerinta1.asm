e9a5d3f92f1c68124f0b91d4fca07bf50cd061d4
Nistor Gheorghe <nistorgeorge404@gmail.com>
2021-11-14 19:40:49 +0200

    hex to bin

--- cerinta-1/cerinta1.asm
+++ cerinta-1/cerinta1.asm
@@ -1,8 +1,16 @@
 //cerinta1
 .data
 	input: .space 100
+	current: .space 100
+	
+	strEmpty: .asciz ""
+
 	formatScanf: .asciz "%s"
-	formatPrintf: .asciz "Sirul citit este %s.\n"
+	formatPrintf: .asciz "%s\n"
+
+	trei: .long 3
+	indexCurrent: .long 0
+	count: .long 0
 .text
 
 .global main
@@ -14,16 +22,335 @@ main:
 	call scanf
 	popl %ebx
 	popl %ebx
+
+	movl $input, %edi
+	movl $current, %esi
+	xor %ecx, %ecx
+et_for:
+	movb (%edi, %ecx, 1), %al
+
+	cmp $0, %al
+	je et_exit
 	
-	//printf("Sirul este %s\n", x);
-	pushl $input
+	cmp $48, %al
+	je cifra_0
+	cmp $49, %al
+	je cifra_1
+	cmp $50, %al
+	je cifra_2
+	cmp $51, %al
+	je cifra_3
+	cmp $52, %al
+	je cifra_4
+	cmp $53, %al
+	je cifra_5
+	cmp $54, %al
+	je cifra_6
+	cmp $55, %al
+	je cifra_7
+	cmp $56, %al
+	je cifra_8
+	cmp $57, %al
+	je cifra_9
+	cmp $65, %al
+	je cifra_A
+	cmp $66, %al
+	je cifra_B
+	cmp $67, %al
+	je cifra_C
+	cmp $68, %al
+	je cifra_D
+	cmp $69, %al
+	je cifra_E
+	cmp $70, %al
+	je cifra_F
+
+et_counter:
+	incl %ecx
+	incl count
+	cmp $3, count
+	je et_resetCurrent
+	jmp et_for
+	
+et_resetCurrent:
+	pushl %ecx
+	
+	pushl $current
 	pushl $formatPrintf
 	call printf
 	popl %ebx
 	popl %ebx
 	
+	pushl $strEmpty
+	pushl $current
+	call strcpy
+	popl %ebx
+	popl %ebx
+	
+	popl %ecx
+	
+	movl $0, indexCurrent
+	movl $0, count
+	jmp et_for	
 
+cifra_0:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_1:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_2:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_3:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_4:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_5:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_6:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_7:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_8:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_9:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_A:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_B:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_C:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_D:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_E:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $48, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
+cifra_F:
+	pushl %ecx
+	
+	movl indexCurrent, %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	movb $49, (%esi, %ecx, 1)
+	incl %ecx
+	addl $4, indexCurrent
+	
+	popl %ecx
+	jmp et_counter
 et_exit:
 	mov $1, %eax
 	xor %ebx, %ebx
 	int $0x80
+
