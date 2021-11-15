44465e747b62a89a503450497ad297f596279848
Nistor Gheorghe <nistorgeorge404@gmail.com>
2021-11-15 19:43:20 +0200

    cerinta2 done

--- cerinta-2/cerinta2.asm
+++ cerinta-2/cerinta2.asm
@@ -1,7 +1,12 @@
 //cerinta2
 .data
-	str: .space 100
-	formatPrintf: .asciz "%d"
+	str: .space 200
+	formatDec: .asciz "%d\n"
+	chDelim: .asciz " "
+	res: .space 4
+	
+	x: .long 0
+	y: .long 0
 .text
 
 .global main
@@ -10,7 +15,92 @@ main:
 	pushl $str
 	call gets
 	popl %ebx
+	
+	pushl $chDelim
+	pushl $str
+	call strtok 
+	popl %ebx
+	popl %ebx
+	
+	pushl %eax
+	call atoi
+	popl %ebx
+	
+	pushl %eax
+et_for:
+	pushl $chDelim
+	pushl $0
+	call strtok
+	popl %ebx
+	popl %ebx 
+	
+	cmp $0, %eax
+	je et_exit
+
+	movl %eax, res	
+	
+	pushl %eax
+	call atoi
+	popl %ebx
+
+	
+	cmp $0, %eax
+	jne et_numar
+	
+	movl res, %edi
+	xor %ecx, %ecx
+	movb (%edi, %ecx, 1), %bl
+	
+	cmp $97, %bl
+	je op_add
+	cmp $115, %bl
+	je op_sub
+	cmp $109, %bl
+	je op_mul
+	cmp $100, %bl
+	je op_div
+op_add:
+	popl y
+	popl x
+	movl x, %eax
+	add y, %eax
+	pushl %eax
+	jmp et_for
+op_sub:
+	popl y
+	popl x
+	movl x, %eax
+	sub y, %eax
+	pushl %eax
+	jmp et_for
+op_mul:
+	popl y
+	popl x
+	movl x, %eax
+	mull y
+	pushl %eax
+	jmp et_for
+op_div:
+	popl y
+	popl x
+	xor %edx, %edx
+	movl x, %eax
+	movl y, %ebx
+	divl %ebx
+	pushl %eax
+	jmp et_for
+et_numar:
+	pushl %eax
+	jmp et_for	
 et_exit:
+	popl %eax
+	
+	pushl %eax
+	pushl $formatDec
+	call printf
+	popl %ebx
+	popl %ebx
+	
 	pushl $0
 	call fflush
 	popl %ebx
